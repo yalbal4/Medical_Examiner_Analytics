3c030af56371f155a6e06e8dd4af83cc094d308d
Yallen <yallenbai@gmail.com>
2024-12-16 18:48:21 -0500

    add year animation to map

--- utils/maps.py
+++ utils/maps.py
@@ -1,72 +1,69 @@
-import dash_leaflet as dl
-import dash_leaflet.express as dlx
-from dash import Dash, html
+from dash.dependencies import Input, Output
+from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 import json
 from shapely.geometry import shape
 
-data = pd.read_csv("conneticut_wide_form.csv")
+data_file_path = 'conneticut_wide_form.csv'
+counties_geojson_file_path = 'CT_Counties.geojson'
+geolocation_column_name = 'injurycitygeo'
+
+data = pd.read_csv(data_file_path)
 
 # data preprocessing
-data = data.dropna(subset='deathcitygeo')
-data['deathcitygeo'] = data['deathcitygeo'].str.replace("'", '"')
-data['geometry'] = data['deathcitygeo'].apply(lambda x: shape(json.loads(x)))
-geodata = gpd.GeoDataFrame(data, geometry='geometry')
+data = data.dropna(subset=geolocation_column_name)
+data[geolocation_column_name] = data[geolocation_column_name].str.replace("'", '"')
+data['geometry'] = data[geolocation_column_name].apply(lambda x: shape(json.loads(x)))
+death_geodata = gpd.GeoDataFrame(data, geometry='geometry', crs=4326)
 
-# data["deathcitygeo"] = data["deathcitygeo"].astype(str)
-# data[['longitude', 'latitude']] = data["deathcitygeo"].str.extract(r'\[([\d\.-]+),\s*([\d\.-]+)\]')
-# data['latitude'] = pd.to_numeric(data['latitude'])
-# data['longitude'] = pd.to_numeric(data['longitude'])
-
-# data_head100 = data.head(100)
-
-# geodicts = [dict(lat=row['latitude'], lon=row['longitude']) for _, row in data_head100.iterrows()]
-# data_geojson = dlx.dicts_to_geojson(geodicts)
-
-with open('CT_Counties.geojson', 'r') as f:
+# CT counties geodata
+with open(counties_geojson_file_path, 'r') as f:
     counties_geojson = json.load(f)
+counties_gdf = gpd.read_file(counties_geojson_file_path)
 
-counties_gdf = gpd.read_file('CT_Counties.geojson')
+joined_gdf = gpd.sjoin(death_geodata, counties_gdf, how='left', predicate='within')
 
-test_data = counties_gdf[['OBJECTID']]
-test_data['value'] = range(len(counties_gdf))
-test_data['value'] += 10
 
-joined_gdf = gpd.sjoin(geodata, counties_gdf, how='left', predicate='within')
+def group_deaths_by_county_and_year(df):
+    df = joined_gdf.groupby(['OBJECTID', 'death_year']).size()
+    df = df.reset_index()
+    df = pd.merge(df, counties_gdf, on='OBJECTID', how='left')
+    df = df.rename(columns={0: 'death_count'})
+    return df
 
-deaths_by_county = joined_gdf.groupby('OBJECTID').size()
-deaths_by_county = deaths_by_county.reset_index()
-deaths_by_county = pd.merge(deaths_by_county, counties_gdf, on='OBJECTID', how='left')
-deaths_by_county = deaths_by_county.rename(columns={0: 'death_count'})
-print(deaths_by_county)
+def create_choropleth_fig(df):
+    fig = px.choropleth(
+        df,
+        geojson=counties_geojson,
+        locations='OBJECTID',
+        color='death_count',
+        scope='usa',
+        hover_name='COUNTY',
+        hover_data={
+            "OBJECTID": False,
+            "death_count": False,
+            "Death Count": df['death_count']},
+        animation_frame='death_year'
+    )
+    return fig
 
-fig = px.choropleth(
-    deaths_by_county,
-    geojson=counties_geojson,
-    locations='OBJECTID',
-    color='death_count',
-    scope='usa',
-    hover_name='COUNTY',
-    hover_data = {
-        "OBJECTID": False,
-        "death_count": False,
-        "Death Count": deaths_by_county['death_count']}
-)
+deaths_by_county = group_deaths_by_county_and_year(joined_gdf)
+
+fig = create_choropleth_fig(deaths_by_county)
 
 # Update layout for map visualization
 fig.update_geos(fitbounds="locations", visible=True)
 fig.update_layout(title="Medical Examiner Deaths by County")
 
-fig.show()
-# app = Dash()
-# app.layout = html.Div([
-#     dl.Map(children=[
-#         dl.TileLayer(),
-#         dl.GeoJSON(data=data_geojson)
-#     ], style={'height': '50vh'}, center=[data_head100['latitude'].median(), data_head100['longitude'].median()], zoom=6)
-# ])
+# fig.show()
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+app = Dash()
+
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
